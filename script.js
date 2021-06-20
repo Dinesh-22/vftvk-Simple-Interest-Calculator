d73bef07fcdb0dbd03a846f20edb310f01208f89
Dinesh-22 <86187157+Dinesh-22@users.noreply.github.com>
2021-06-20 17:56:18 +0530

    Simple Interest calculator
    My version of simple interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var rate;
+function myfn(){
+   var x = document.getElementById("amt").value;
+   var z = document.getElementById("select").value;
+   var p = Number(x);
+   if(p<0)
+  {
+   alert("Enter a positive number");
+  } 
+   var r = Number(z);
+//    if(document.getElementById("result").style.display=="none")
+//   {document.getElementById("result").style.display="block";}
+   var res = (p*rate*r)/100;
+   document.getElementById("result").innerHTML = " "+String(res);
 }
-        
+function updateTextInput(val) {
+   rate = val;
+   document.getElementById('textInput').innerHTML = rate + "%";
+ }
